2189cc1d2ff2e748c051514a11c761a391be38d4
harshit <upadhyayharshit05@gmail.com>
2025-06-10 15:04:58 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,304 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './App.css';
+
+import { FaMinus } from 'react-icons/fa';
+import { MdAdd, MdDone, MdKeyboardArrowUp, MdOutlineKeyboardArrowDown } from 'react-icons/md';
+import Loader from './Loader';
+
+
+const getTime = () =>
+  new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+const Chatbot = () => {
+  const messageRef = useRef(null);
+  const scrollToBottom = () => {
+    messageRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const [messages, setMessages] = useState([
+    {
+      id: Date.now(),
+      type: 'bot',
+      text: "Hi, I'm Sarah,\nHow can I help today ?",
+      time: getTime(),
+    },
+    {
+      id: Date.now(),
+      type: 'bot',
+      text: "Are you looking Forward for -:",
+      options: [
+        "A Death has Occurred",
+        "I am Planning Ahead",
+        "Find an Obituary",
+        "I have a general enquiry"
+      ],
+      time: getTime(),
+    }
+  ]);
+
+  const [input, setInput] = useState('');
+  const [showHeader, setShowHeader] = useState(true);
+  const [botVisible, setBotVisible] = useState(true);
+  // const [contactId,setContactId]=useState(()=>{
+  //   return localStorage.getItem('mortem_contact_id') || null ;
+  // })
+  const [contactId, setContactId] = useState(null);
+
+
+  // adding a unique string inn the last_name 
+  const getIPAddress = async () => {
+    try {
+      const res = await fetch("https://api.ipify.org?format=json");
+      const data = await res.json();
+      return data.ip;
+    } catch {
+      return "unknown_ip";
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const sendMessage = async (buttonText) => {
+    const userMessage = (buttonText ?? input).trim();
+    if (!userMessage) return;
+    const userTime = getTime();
+    const userId = Date.now();
+    const loaderId = "loader-" + Date.now();
+
+    // setMessages(prev => [
+    //   ...prev,
+    //   { id: userId, type: 'user', text: userMessage, time: userTime }
+    // ]);
+    setMessages(prev => [
+      ...prev,
+      { id: userId, type: 'user', text: userMessage, time: userTime },
+      { id: loaderId, type: 'bot', loading: true, text: '', time: getTime() }
+    ]);
+    setInput('');
+
+    const ip = await getIPAddress();
+    const basePayload = {
+      // payload data
+      tags: "ai response,immediate need - entry",
+      country: "US",
+      message: {
+        body: userMessage,
+        type: 29,
+      },
+      location: {
+        id: "anzT2So2oHAxu8AgUsf9",
+        city: "Altoona",
+        name: "Mortem AI ",
+        state: "WI",
+        address: "3023 N. 52nd Avenue East1717 Devney Dr, Altoona, ",
+        country: "US",
+        postalCode: "54720",
+        fullAddress: "3023 N. 52nd Avenue East1717 Devney Dr, Altoona, , Altoona WI 54720",
+      },
+      workflow: {
+        id: "b4e65058-83bd-460b-8a2c-255068af687c",
+        name: "Customer Replied > Set Channel > Send Webhook",
+      },
+      full_name: "Guest Visitor qxdxg",
+      last_name: `Visitor_${ip}`,
+      customData: {},
+      first_name: "Guest",
+      triggerData: {},
+      contact_type: "lead",
+      date_created: new Date().toISOString(),
+      full_address: "",
+      Inbound_Reply: userMessage,
+      Message_Reply: "",
+      Reply_Channel: "Live_Chat",
+      PreNeed_Location: "",
+      Conversation_History: "",
+      Immediate_Need_Location: ""
+    };
+    // first time we dont have cotact_id 
+    const payload = contactId ? { ...basePayload, contact_id: contactId } : basePayload
+
+    try {
+      const res = await fetch('http://54.204.221.74/api/inbound/?bot=asst_zkMH7Fjxv166GWoqlYj20C1t', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload),
+      });
+
+      const text = await res.text();
+      let botReply;
+      let returnedId;
+      console.log("text", JSON.parse(text))
+
+      try {
+        const data = JSON.parse(text);
+        botReply = data['reply'] || 'Sorry, something went wrong.';
+        returnedId = data?.contact_id;
+
+        // if(! contactId &&  returnedId){
+        //   setContactId(returnedId)
+        //   localStorage.setItem('mortem_contact_id',returnedId)
+        // }
+        if (!contactId && returnedId) {
+          setContactId(returnedId);
+        }
+      } catch (parseErr) {
+        console.warn('Non-JSON response:', text);
+        botReply = text;
+      }
+
+      // setMessages(prev => [
+      //   ...prev,
+      //   { id: Date.now(), type: 'bot', text: botReply, time: getTime() }
+      // ]);
+      setMessages(prev => prev.map(msg =>
+        msg.id === loaderId
+          ? { ...msg, loading: false, text: botReply }
+          : msg
+      ));
+
+    } catch (err) {
+      console.error('Error:', err);
+      // setMessages(prev => [
+      //   ...prev,
+      //   { id: Date.now(), type: 'bot', text: 'Failed to reach server.', time: getTime() }
+      // ]);
+      setMessages(prev => prev.map(msg =>
+        msg.id === loaderId
+          ? { ...msg, loading: false, text: 'Failed to reach server.' }
+          : msg
+      ));
+    }
+  };
+
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') sendMessage();
+  };
+
+  return (
+    <div className='main-container'>
+      <div className='chatbot-main-container'>
+        {botVisible &&
+          <div className="chatbot-wrapper">
+            <div className="chatbot-header">
+              {showHeader && (
+                <div className='section-1'>
+                  <div style={{ alignSelf: "center" }}>
+                    <img src="./Black.png" alt="" />
+                  </div>
+                  <div>
+                    <h3 style={{ fontSize: "24px", margin: 0 }}>Mortem AI</h3>
+                    <span className="chatbot-status">● Online</span>
+                  </div>
+                </div>
+              )}
+
+              <div className='section-1' style={!showHeader ? { width: "100%" } : {}} >
+
+                {
+                  !showHeader ? (
+                    <div className="circle-toggle" style={!showHeader ? { marginLeft: "auto" } : {}}>
+                      <MdAdd fontSize="16px" color='white' onClick={() => setShowHeader(pre => !pre)} />
+                    </div>
+                  ) : (
+                    <div className="circle-toggle" onClick={() => setShowHeader(pre => !pre)}>
+                      <FaMinus fontSize="10px" />
+
+                    </div>
+                  )
+                }
+              </div>
+            </div>
+
+            <div className="chatbot-body">
+              {messages.map((msg, id) => (
+                <div className={`${msg.type}`} key={id}>
+                  {msg.type === 'bot' ? (
+                    <>
+                      <div className={`talk-bubble tri-right round btm-left`}>
+                        <div className="talktext">{msg.loading ? <Loader /> : msg.text}</div>
+
+                        {
+                          msg.options && !msg.loading && (
+                            <div className='talk-options'>
+                              {msg.options.map((opt, idx) => (
+                                <button key={idx}
+                                  onClick={() => sendMessage(opt)}
+                                >
+                                  {idx + 1}. &nbsp;{opt}
+                                </button>
+                              ))}
+                            </div>
+                          )
+                        }
+
+
+                      </div>
+                      <div className="message-container">
+                        <div className="avatar">
+                          <img src="/logo.png" alt="bot" />
+                        </div>
+                        <div className="message-time-avatar">{msg.time}</div>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className="talk-bubble-user tri-right btm-right">
+                        <div className="talktext talk-user" style={{ color: "rgba(68, 68, 68, 1)" }}>
+                          {msg.text}
+                        </div>
+                      </div>
+                      <div className="message-container-user">
+                        <div className="message-time-user">
+                          {msg.time}&nbsp;<MdDone />
+
+                        </div>
+
+                        <div>
+                          <img src="./user.png" alt="user" />
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </div>
+              ))}
+              <div ref={messageRef} />
+            </div>
+
+            <div className="chatbot-input">
+              <div className='inside-bot'>
+                <input
+                  style={{ fontSize: "18px" }}
+                  type="text"
+                  placeholder="Type your message here..."
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                />
+                <button className="send-button" onClick={sendMessage}>➤</button>
+              </div>
+            </div>
+          </div>
+        }
+
+
+      </div>
+      <div className='close-bot-container'>
+        <button className="close-bot-button" onClick={() => setBotVisible(pre => !pre)}>
+          {
+            botVisible ?
+              <MdOutlineKeyboardArrowDown />
+              :
+             <img src="./Black.png" alt="" />
+          }
+        </button>
+      </div>
+    </div>
+
+  );
+};
+
+export default Chatbot;
+
---
+++ src/Loader.jsx
@@ -0,0 +1,6 @@
+const  Loader = () => (
+    <div className="typing">
+      <span /><span /><span />
+    </div>
+  );
+  export default Loader
